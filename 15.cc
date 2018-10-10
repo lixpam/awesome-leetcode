92b98416827da91b699a898662022fa7f2fd5ca7
lixpam <lixpam@163.com>
2018-10-10 11:24:41 +0800

    update 15 for another method

--- 15.cc
+++ 15.cc
@@ -11,6 +11,8 @@ static const int _ = []() {
     return 0;
 }();
 
+/* double loop + hash
+ * O(N*N)
 class Solution {
 public:
     vector<vector<int>> threeSum(vector<int>& nums) {
@@ -32,6 +34,37 @@ public:
                 if (it != mp.end() && it->second > j) {
                     ret.push_back(vector<int>{nums[i], nums[j], left});
                 } 
+            }
+        }
+        return ret;
+    }
+};
+*/
+
+// O(N*N)
+class Solution {
+public:
+    vector<vector<int>> threeSum(vector<int>& nums) {
+        vector<vector<int>> ret;
+        sort(nums.begin(), nums.end());
+        int len = nums.size();
+        for (int i = 0; i < len-2; ++i) {
+            if (nums[i] > 0) continue;
+            if (i > 0 && nums[i] == nums[i-1]) continue;
+
+            for (int j = i+1, k = len-1; j < k;) {
+                int v = nums[i] + nums[j] + nums[k];
+                if (v > 0) {
+                    k--;
+                } else if (v < 0) {
+                    j++;
+                } else {
+                    ret.push_back(vector<int>{nums[i], nums[j], nums[k]});
+                    while (j < k && nums[k-1] == nums[k]) k--;
+                    while (j < k && nums[j] == nums[j+1]) j++;
+                    j++;
+                    k--;
+                }
             }
         }
         return ret;

7ae575f451cb78aac667ab9a4c3ebccb665b8446
lixpam <lixpam@163.com>
2018-10-21 16:53:13 +0800

    895

---
+++ 895.cc
@@ -0,0 +1,80 @@
+#include <iostream>
+#include <map>
+#include <vector>
+#include <unordered_set>
+#include <unordered_map>
+using namespace std;
+
+class FreqStack {
+private:
+    struct Node {
+        int val;
+        int cnt;
+        Node(int val):val(val),cnt(1){}
+    };
+public:
+    FreqStack() {
+    }
+    
+    void push(int x) {
+        auto it = val2node.find(x);
+        Node *n = NULL;
+        if (it == val2node.end()) {
+            n = new Node(x);
+            val2node[x] = n;
+        } else {
+            n = it->second;
+            n->cnt++;
+        }
+        elems.push_back(n);
+        
+        if (n->cnt != 1) {
+            auto &f = freqlist[n->cnt-1];
+            f.erase(n->val);
+            
+            f = freqlist[n->cnt];
+            f.insert(n->val);
+        }
+    }
+    
+    int pop() {
+        auto &fl = freqlist.begin()->second;
+        for (auto b = elems.rbegin(); b != elems.rend(); --b) {
+            int val = (*b)->val;
+            int cnt = (*b)->cnt;
+            if (fl.find(val) != fl.end()) {
+                auto &f = freqlist[cnt];
+                f.erase(val);
+                if (f.size() == 0) {
+                    freqlist.erase(cnt);
+                }
+                (*b)->cnt--;
+                elems.erase((++b).base());
+                if ((*b)->cnt > 0) {
+                    freqlist[(*b)->cnt].insert(val);
+                } else {
+                    val2node.erase(val);
+                }
+                
+                return val;
+            }
+        }
+    }
+private:
+    vector<Node*> elems;
+    unordered_map<int, Node*> val2node;                 // x->node
+    map<int, unordered_set<int>, greater<int> > freqlist;     // freq->x
+};
+
+int main()
+{
+    FreqStack fs;
+    fs.push(5);
+    fs.push(7);
+    fs.push(5);
+    fs.push(7);
+    fs.push(4);
+    fs.push(5);
+
+    cout << fs.pop() << endl;
+}
